66444d191e273b78e383521b1ad2a855db239277
arielapz <ariellapinas214@gmail.com>
2023-06-08 10:51:46 +0800

    initial commit

---
+++ client/nuxt-lab/app.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div v-for="item in data" :key="item.id">
+      <!-- Display your data here -->
+      <p>{{ item.field1 }}</p>
+      <p>{{ item.field2 }}</p>
+    </div>
+    <button @click="previousPage">Previous</button>
+    <button @click="nextPage">Next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [],
+      currentPage: 1,
+    };
+  },
+  methods: {
+    fetchData() {
+      // Make an API request to the Laravel API endpoint
+      // Include the "currentPage" parameter for pagination
+    },
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        this.fetchData();
+      }
+    },
+    nextPage() {
+      this.currentPage++;
+      this.fetchData();
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
+</script>
